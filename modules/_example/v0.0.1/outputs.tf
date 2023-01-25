3f28a38fe1027025e94198e58603bbd60a32e2e0
Yannick van Rooyen CN <yannick.vanrooyen@cloudnation.nl>
2023-01-25 14:51:07 +0100

    add faux tf code

---
+++ modules/_example/v0.0.1/outputs.tf
@@ -0,0 +1,15 @@
+output "arn" {
+  description = "Infinidash dashboard arn"
+  value       = aws_infinidash_dashboard.example.arn
+}
+
+output "name" {
+  description = "Infinidash dashboard arn"
+  value       = aws_infinidash_dashboard.example.name
+}
+
+output "authkey" {
+  count       = var.optionalresource ? 1 : 0
+  description = "Infinidash auth key"
+  value       = aws_infinidash_auth.example[*].key
+}
