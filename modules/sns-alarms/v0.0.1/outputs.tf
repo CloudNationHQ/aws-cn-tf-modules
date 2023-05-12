2634345a9b6e821fbc49840b03dc9f0aa96eead8
Yannick van Rooyen CN <yannick.vanrooyen@cloudnation.nl>
2023-05-12 12:52:04 +0200

    add output to sns

--- modules/sns-alarms/v0.0.1/outputs.tf
+++ modules/sns-alarms/v0.0.1/outputs.tf
@@ -1,4 +1,9 @@
 output "sns" {
-  description = "SNS ARN"
+  description = "SNS ARN - Legacy"
+  value       = aws_sns_topic.sns.arn
+}
+
+output "topic_arn" {
+  description = "SNS Topic ARN"
   value       = aws_sns_topic.sns.arn
 }
