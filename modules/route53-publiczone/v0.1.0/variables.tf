ac76b4f99ad443adfa2a52e7b31db14bb4f52972
Yannick van Rooyen CN <yannick.vanrooyen@cloudnation.nl>
2023-06-06 10:36:18 +0200

    add dnssec records

---
+++ modules/route53-publiczone/v0.1.0/variables.tf
@@ -0,0 +1,15 @@
+variable "public_zone_name" {
+  type        = string
+  description = "Name for the public zone, e.g. example.com"
+}
+
+variable "tags" {
+  type        = map(any)
+  default     = null
+  description = "tags"
+}
+
+variable "dnssec" {
+  type    = bool
+  default = false
+}
