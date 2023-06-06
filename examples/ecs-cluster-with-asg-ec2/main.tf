ee754088a8d9c081ebbf69107e45a1a7184cbc94
Sebastiaan Brozius <s.brozius@gmail.com>
2023-06-06 18:20:03 +0200

    Added second AZ which is needed for the database subnets Added desired_capacity to the ecs module Referenced new commit for the desired_capacity

--- examples/ecs-cluster-with-asg-ec2/main.tf
+++ examples/ecs-cluster-with-asg-ec2/main.tf
@@ -13,10 +13,10 @@ module "vpc" {
   source             = "github.com/CloudNation-nl/aws-terraform-modules//modules/vpc/v2.1.0"
   cidr_range         = "10.13.0.0/16"
   env                = local.env
-  azs                = ["eu-west-1a"]
-  public_subnets     = ["10.13.1.0/24"]
-  private_subnets    = ["10.13.2.0/24"]
-  database_subnets   = ["10.13.3.0/24"]
+  azs                = ["eu-west-1a", "eu-west-1b"]
+  public_subnets     = ["10.13.1.0/24", "10.13.2.0/24"]
+  private_subnets    = ["10.13.3.0/24", "10.13.4.0/24"]
+  database_subnets   = ["10.13.5.0/24", "10.13.6.0/24"]
   enable_nat_gateway = false
 }
 
@@ -39,8 +39,11 @@ resource "aws_security_group" "this" {
 }
 
 module "ecs_asg_ec2" {
-  source             = "github.com/CloudNation-nl/aws-terraform-modules//modules/ecs-cluster-ec2-asg/v0.0.1?ref=affb6f0bed8791157ececd779f8feed738afc4ac"
+  source             = "github.com/CloudNation-nl/aws-terraform-modules//modules/ecs-cluster-ec2-asg/v0.0.1?ref=9468160b2fcd3e84be35dd173cb95ca45972d8c0"
   cluster_name       = "ecs_cluster_asg_sebro"
   subnet_ids         = module.vpc.subnet_private_subnet_ids
   security_group_ids = [aws_security_group.this.id]
+  min_size           = 0
+  max_size           = 2
+  desired_capacity   = 1
 }
