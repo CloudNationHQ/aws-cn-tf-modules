eea2494535df3e0dc96a0357f821fe9dc0a45b0b
Hugo van Duijn <hugo.vanduijn@cloudnation.nl>
2023-07-20 11:37:23 +0200

    Route53 publiczone add feat caa (#30)
    * added v0.2.0 of route53 module
    
    * fix readme
    
    * fix readme
    
    * fix readme
    
    * Update variables.tf
    
    Lint fix

---
+++ modules/route53-publiczone/v0.2.0/variables.tf
@@ -0,0 +1,31 @@
+variable "public_zone_name" {
+  type        = string
+  description = "Name for the public zone, e.g. example.com"
+}
+
+variable "tags" {
+  type        = map(any)
+  default     = null
+  description = "tags"
+}
+
+variable "CAA" {
+  type = list(string)
+  default = [
+    "0 issue \"amazon.com\"",
+    "0 issuewild \"amazon.com\""
+  ]
+  description = "CAA records for domain, set [] for no CAA records"
+}
+
+variable "dnssec" {
+  type        = bool
+  default     = false
+  description = "Enable DNSSEC"
+}
+
+variable "force_destroy" {
+  type        = bool
+  default     = false
+  description = "Force destroy of all records when zone is deleted"
+}
